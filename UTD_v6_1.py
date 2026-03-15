2ff9fc6422c0967bb8906c2458aff8794b6e5cd8
SYS159 <youngseok159@naver.com>
2026-03-15 22:13:48 +0900

    260315_2200

--- UTD_v6_1.py
+++ UTD_v6_1.py
@@ -26,8 +26,8 @@ strategy_config = {
 "KRW-AVAX": {"rsi_threshold": 25, "rsi_max": 40, "ts_activation": 1.0, "use_rsi_drop": True, "vol_factor": 1.5},
 "KRW-DOGE": {"rsi_threshold": 25, "rsi_max": 50, "ts_activation": 1.5, "use_rsi_drop": False, "vol_factor": 2.0},
 "KRW-LINK": {"rsi_threshold": 25, "rsi_max": 40, "ts_activation": 1.0, "use_rsi_drop": False, "vol_factor": 2.0},
-"KRW-NEAR": {"rsi_threshold": 25, "rsi_max": 50, "ts_activation": 1.5, "use_rsi_drop": False, "vol_factor": 1.5},
-"KRW-SHIB": {"rsi_threshold": 25, "rsi_max": 50, "ts_activation": 1.5, "use_rsi_drop": False, "vol_factor": 1.0}
+"KRW-NEAR": {"rsi_threshold": 25, "rsi_max": 50, "ts_activation": 1.5, "use_rsi_drop": False, "vol_factor": 1.5}
+# "KRW-SHIB": {"rsi_threshold": 25, "rsi_max": 50, "ts_activation": 1.5, "use_rsi_drop": False, "vol_factor": 1.0}
 }
 
 target_tickers = list(strategy_config.keys())
